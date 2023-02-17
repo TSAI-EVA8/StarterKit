bf32ff7d2c09b76f4110e9c9bba0d873fb245ecd
Rohit Agarwal <>
2023-02-17 18:26:12 +0100

    added the lr_finder function

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torchvision
 from torchsummary import summary
-
+from torch_lr_finder import LRFinder
 
 
 
@@ -156,6 +156,24 @@ def sgd_optimizer(model, learning_rate, momentum, l2_factor=0.0):
         model.parameters(), lr=learning_rate, momentum=momentum, weight_decay=l2_factor
     )
 
+
+def find_lr(net, optimizer, criterion, train_loader):
+    """Find learning rate for using One Cyclic LRFinder
+    Args:
+        net (instace): torch instace of defined model
+        optimizer (instance): optimizer to be used
+        criterion (instance): criterion to be used for calculating loss
+        train_loader (instance): torch dataloader instace for trainig set
+    """
+    lr_finder = LRFinder(net, optimizer, criterion, device="cuda")
+    lr_finder.range_test(train_loader, end_lr=10, num_iter=200, step_mode="exp")
+    lr_finder.plot()
+    min_loss = min(lr_finder.history['loss'])
+    ler_rate = lr_finder.history['lr'][np.argmin(lr_finder.history['loss'], axis=0)]
+    print("Max LR is {}".format(ler_rate))
+
+    lr_finder.reset()
+    return format(ler_rate)
 
 
 def class_level_accuracy(model, loader, device, classes):
